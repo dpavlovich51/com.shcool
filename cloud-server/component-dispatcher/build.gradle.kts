f43186a73d4198c968260834da94225e93b3b33f
dpavlovich <dpavlovich51@gmail.com>
2024-05-19 20:42:39 +0300

    added cloud-server module

---
+++ cloud-server/component-dispatcher/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.school"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	runtimeOnly("com.h2database:h2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
 rootProject.name = "school"
 
 include("student")
+include("cloud-server")
+include("cloud-server:component-dispatcher")
+include("cloud-server:cloud-config")
